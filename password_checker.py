72c165406341c5d75a3567697c772d273c37806f
pauljeremiah2k6-a11y <pauljeremiah2k6@gmail.com>
2026-06-11 16:23:35 +0530

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -1,24 +1,16 @@
 import re
-
 password = input("Enter Password: ")
-
 score = 0
-
 if len(password) >= 8:
     score += 1
-
 if re.search("[A-Z]", password):
     score += 1
-
 if re.search("[a-z]", password):
     score += 1
-
 if re.search("[0-9]", password):
     score += 1
-
 if re.search("[!@#$%^&*]", password):
     score += 1
-
 if score <= 2:
     print("Weak Password")
 elif score <= 4:
