cef20ef63a2c40bd1141dd43ef084eb5050e7b7a
pauljeremiah2k6-a11y <pauljeremiah2k6@gmail.com>
2026-06-11 16:24:10 +0530

    Update port_scanner.py

--- port_scanner.py
+++ port_scanner.py
@@ -1,28 +1,19 @@
 import socket
-
 def scan_port(target, port):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)  # 1 second timeout
-
         result = sock.connect_ex((target, port))
-
         if result == 0:
             print(f"[OPEN] Port {port}")
         else:
             print(f"[CLOSED] Port {port}")
-
         sock.close()
-
     except socket.gaierror:
         print("Hostname could not be resolved")
     except socket.error:
         print("Could not connect to server")
-
-
 target = input("Enter target IP or website (example: 127.0.0.1): ")
-
 print("\nScanning ports 1 to 100...\n")
-
 for port in range(1, 101):
     scan_port(target, port)
